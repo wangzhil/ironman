b35fe9dbf9b1b2e95496d61760319f1a1f12f16f
wangzhilei <developerwang@foxmail.com>
2021-08-28 08:55:34 +0800

    基础外壳

---
+++ gof23/src/main/java/com/wzl/gof23/Gof23Application.java
@@ -0,0 +1,18 @@
+package com.wzl.gof23;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author: wangzhilei
+ * @version: 1.0.0
+ * @date: 2021/8/27 23:56
+ */
+@SpringBootApplication
+public class Gof23Application {
+
+    public static void main(String[] args) {
+
+    }
+}
